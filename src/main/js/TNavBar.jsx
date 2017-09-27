d99f18a473700caf981e31cf6a581dbc3d7e31c9
Raghavendra Shanbhag <ragshanb@cisco.com>
2017-09-27 09:14:55 +0530

    Minor Blog name change

--- src/main/js/TNavBar.jsx
+++ src/main/js/TNavBar.jsx
@@ -99,7 +99,7 @@ class TNavBar extends React.Component {
 			<Navbar inverse collapseOnSelect fixedTop>
 				<Navbar.Header  >
 					<Navbar.Brand >
-						<a href="#"  onClick={this.onHomeSelect}>Savi Savi Nenapu Blog new</a>
+						<a href="#"  onClick={this.onHomeSelect}>Savi Savi Nenapu Blog</a>
 					</Navbar.Brand>
 					<Navbar.Toggle />
 				</Navbar.Header>
